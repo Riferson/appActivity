5c5d23dfbd2aa0e4a7279187d150bd91f61f9e12
Riferson <riferson_21@outlook.com>
2023-10-13 12:23:25 -0300

    Adicinando o modal

--- src/pages/Galeria/index.tsx
+++ src/pages/Galeria/index.tsx
@@ -1,18 +1,24 @@
 import {Container,Title,ContainerImagens,ContainerCamera,ScrollViewImagens,ContainerImg,Img,ContainerStyleImg,ContainerHead} from './styled';
 import React, { useState,useEffect } from "react";
-import { TouchableOpacity,Text } from "react-native";
+import { TouchableOpacity,Text, View,Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useDataContext } from '../../Context/DataContext';
 import { FontAwesome } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons'; 
-
+import Modal from 'react-native-modal';
 export default function Galeria(){
     const { objetos } = useDataContext(); 
-
+    const [isModalVisible, setModalVisible] = useState(false);
+    const [objetoClicado, setObjetoClicado] = useState(null);
     const navigation = useNavigation();
      function handleCameraOpen(){
         navigation.navigate('camera');
      }
+
+     const abrirModal = (objeto:any) => {
+        setObjetoClicado(objeto);
+        setModalVisible(true);
+      };
 
     return(
     <Container>
@@ -27,9 +33,9 @@ export default function Galeria(){
             <ScrollViewImagens>
                 <ContainerImg>
                     { objetos && objetos.length>0? (objetos.map((objeto, index) => (
-                        <ContainerStyleImg key={index}>
+                        <TouchableOpacity onPress={() => abrirModal(objeto)} key={index}>
                             <Img source={{ uri: objeto.uri }} />
-                         </ContainerStyleImg>
+                         </TouchableOpacity>
                     ))) : (<Text>Nenhum imagem disponível.</Text>)}
  
                 </ContainerImg>
@@ -38,6 +44,26 @@ export default function Galeria(){
         <ContainerCamera>
             <TouchableOpacity  onPress={handleCameraOpen}><FontAwesome name="camera-retro" size={42} color="black" /></TouchableOpacity>
         </ContainerCamera>
+        <Modal isVisible={isModalVisible} backdropColor="black">
+      <View>
+      {objetoClicado ? (
+        <>
+            <Image
+              source={{ uri: objetoClicado.uri }}
+              style={{ width: '100%', height: '90%' }}
+            />
+            <TouchableOpacity onPress={() => setModalVisible(false)}>
+          <Text>Fechar</Text>
+        </TouchableOpacity>
+        </>
+            
+          ) : (
+            <Text>Imagem não disponível</Text>
+            
+          )}
+        
+      </View>
+    </Modal>
     </Container>
     );
 }
